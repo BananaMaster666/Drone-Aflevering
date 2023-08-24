82d001e5f48b4e8409142b4d6c876623fc774aac
BananaMaster666 <draugiem92@gmail.com>
2023-08-24 09:15:39 +0200

    #2
    Virker stadig ikke.

--- Drone Afleverings opgave/Drone Afleverings opgave/Program.cs
+++ Drone Afleverings opgave/Drone Afleverings opgave/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.ComponentModel.DataAnnotations;
 
 namespace DronerOpgave
 {
@@ -12,28 +13,28 @@ namespace DronerOpgave
 
         static void Main(string[] args)
         {
-            File.Delete("text.csv");
+            File.Delete("doneText.csv");
             string theText = File.ReadAllText("flyvning1.csv");
-            Console.WriteLine(theText);
 
-
-            String[] spearator = { ";" };
+            String[] spearator = { ";", "\n" };
             Int32 count = int.MaxValue;
 
             // using the method
             String[] strlist = theText.Split(spearator, count,
                    StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (String s in strlist)
-            {
-                Console.WriteLine(s);
-                string[] lines = new string[s.Length];
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    lines[i] = s;
-                }
 
-            }
+        for (int i = 0; i < strlist.Length; i++)
+        {
+         Console.WriteLine(strlist[i]);
+         File.AppendAllText("doneText.csv", strlist[i]+"\n");
+        }
+
+                                
+            for(int i = 17; i < strlist.Length; i++)
+            {
+            string longData[] = new string[];
+            }     
 
         }
     }
