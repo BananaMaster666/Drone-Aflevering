afe7bafc047935e677e818c044acb33f643b0b0f
BananaMaster666 <draugiem92@gmail.com>
2023-08-25 14:48:20 +0200

    Skift til Lister
    Skift til lister.
    
    Læst og gemt data.
    Rettet Data.
    Begyndt på at Samle data

--- Drone Afleverings opgave/Drone Afleverings opgave/Program.cs
+++ Drone Afleverings opgave/Drone Afleverings opgave/Program.cs
@@ -19,23 +19,63 @@ namespace DronerOpgave
             String[] spearator = { ";", "\n" };
             Int32 count = int.MaxValue;
 
-            // using the method
+            // Teksten bliver splittet
             String[] strlist = theText.Split(spearator, count,
                    StringSplitOptions.RemoveEmptyEntries);
 
-
+        // Teksten bliver læst
         for (int i = 0; i < strlist.Length; i++)
         {
-         Console.WriteLine(strlist[i]);
          File.AppendAllText("doneText.csv", strlist[i]+"\n");
         }
 
-                                
+        // Longittude data læsning
+    List<string> longtitudeData = new List<string>();                 
             for(int i = 17; i < strlist.Length; i++)
             {
-            string longData[] = new string[];
-            }     
+            string brokenLongData = strlist[i];
+           // Console.WriteLine("long data " + i + ":" + brokenLongData);
+       longtitudeData.Add(brokenLongData);
+            i = (i+12);
+            }   
 
+
+        // Latitude data læsning 
+    List<string> latitudeData = new List<string>();                   
+            for(int i = 18; i < strlist.Length; i++)
+            {
+            string brokenLatData = strlist[i];
+           // Console.WriteLine("lat data " + i + ":" + brokenLatData);
+         latitudeData.Add(brokenLatData);
+            i = (i+12);
+            }    
+
+        // Longtitude data Rettelse
+            for(int i = 0; i < longtitudeData.Count; i++)
+            {
+            longtitudeData[i] = longtitudeData[i].Replace(".", "");
+            longtitudeData[i] = longtitudeData[i].Insert(1, ".");
+            //Console.WriteLine("Rettede Long Data #" + i + ": " + longtitudeData[i]);
+            }
+
+         // Latitude data Rettelse
+            for(int i = 0; i < latitudeData.Count; i++)
+            {
+            latitudeData[i] = latitudeData[i].Replace(".", "");
+            latitudeData[i] = latitudeData[i].Insert(2, ".");
+            //Console.WriteLine("Rettede Lat Data #" + i + ": " + latitudeData[i]);
+            }
+
+
+
+        //samle dit data
+
+    List<string> samledeData = new List<string>();
+            for (int i = 0; i < samledeData.Count; i++)
+            {
+                samledeData.Add("(" + longtitudeData[i] + ";" + latitudeData[i] + ")");
+                Console.WriteLine(samledeData[i]);
+            } 
         }
     }
 }
